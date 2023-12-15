edda9b914497a511de8e493ed19d7ed7a714c294
Eduardo Aviles <eduardo.aviles@loanpro.io>
2023-12-14 21:21:11 -0600

    Add step target to Makefile

--- {{cookiecutter.repo_name}}/main.py
+++ {{cookiecutter.repo_name}}/main.py
@@ -2,6 +2,7 @@ import json
 import os
 import tempfile
 
+import dotenv
 import hydra
 import mlflow
 import wandb
@@ -15,39 +16,59 @@ _steps = [
     'preprocessing',
 ]
 
-
+# Load environment variables
+dotenv.load_dotenv()
 # This automatically reads in the configuration
+
+
 @hydra.main(config_name='config')
 def go(config: DictConfig):
 
     # Steps to execute
     steps_par = config['main']['steps']
     active_steps = steps_par.split(",") if steps_par != "all" else _steps
-
+    root_path = hydra.utils.get_original_cwd()
+    config_path = os.path.join(
+        root_path,
+        "config.yaml"
+    )
+    data_path = os.path.join(
+        root_path,
+        "data"
+    )
+    raw_data = os.path.join(
+        data_path,
+        'raw'
+    )
+    processed_data = os.path.join(
+        data_path,
+        'processed'
+    )
     # Move to a temporary directory
-    with tempfile.TemporaryDirectory() as tmp_dir:
+    with mlflow.start_run(run_name=f" {config['main']['project_name']} - {config['main']['experiment_name']}"):
+        with tempfile.TemporaryDirectory() as tmp_dir:
 
-        if "get_data" in active_steps:
-            # Download file and load in W&B
-            pass
+            if "get_data" in active_steps:
+                # Download file and load in W&B
+                pass
 
-        if "basic_cleaning" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            if "basic_cleaning" in active_steps:
+                ##################
+                # Implement here #
+                ##################
+                pass
 
-        if "data_check" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            if "data_check" in active_steps:
+                ##################
+                # Implement here #
+                ##################
+                pass
 
-        if "preprocessing" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            if "preprocessing" in active_steps:
+                ##################
+                # Implement here #
+                ##################
+                pass
 
 
 if __name__ == "__main__":
